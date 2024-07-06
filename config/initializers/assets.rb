407d2b82ced8a27930eb6f315052904121bb5214
Ilya Dyaglev <ilya.dyaglev@gmail.com>
2024-07-06 02:04:11 +0200

    assets compilation fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
 Rails.application.config.assets.precompile += %w( application.js register_controllers.js application.css )
+Rails.application.config.assets.precompile += %w( badges/badge1.png badges/badge2.png badges/badge3.png badges/badge4.png badges/badge5.png badges/badge6.png badges/badge7.png )
